c066963b50cc008c6bd56093578842e28a778d21
Jerry Xiong <jxiong21029@gmail.com>
2025-05-15 17:48:19 -0400

    add env_test.py, vpt/

---
+++ vpt/lib/mlp.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+from .lib import misc
+from .lib import torch_util as tu
+
+
+class MLP(nn.Module):
+    def __init__(self, insize, nhidlayer, outsize, hidsize, hidactiv):
+        super().__init__()
+        self.insize = insize
+        self.nhidlayer = nhidlayer
+        self.outsize = outsize
+        in_sizes = [insize] + [hidsize] * nhidlayer
+        out_sizes = [hidsize] * nhidlayer + [outsize]
+        self.layers = nn.ModuleList(
+            [
+                tu.NormedLinear(insize, outsize)
+                for (insize, outsize) in misc.safezip(in_sizes, out_sizes)
+            ]
+        )
+        self.hidactiv = hidactiv
+
+    def forward(self, x):
+        *hidlayers, finallayer = self.layers
+        for layer in hidlayers:
+            x = layer(x)
+            x = self.hidactiv(x)
+        x = finallayer(x)
+        return x
+
+    @property
+    def output_shape(self):
+        return (self.outsize,)
---
+++ vpt/lib/torch_util.py
@@ -0,0 +1,120 @@
+import functools
+
+import torch as th
+from torch import nn
+
+
+def contextmanager_to_decorator(cm):
+    def decorator(fn):
+        @functools.wraps(fn)
+        def newfn(*args, **kwargs):
+            with cm():
+                return fn(*args, **kwargs)
+
+        return newfn
+
+    return decorator
+
+
+def have_cuda():
+    return th.cuda.is_available()
+
+
+def default_device_type():
+    return "cuda" if have_cuda() else "cpu"
+
+
+no_grad = contextmanager_to_decorator(th.no_grad)
+DEFAULT_DEVICE = th.device(type=default_device_type())
+
+
+def set_default_torch_device(device):
+    global DEFAULT_DEVICE
+    DEFAULT_DEVICE = th.device(device)
+
+
+def dev():
+    return DEFAULT_DEVICE
+
+
+def zeros(*args, **kwargs):
+    return th.zeros(*args, **kwargs, device=dev())
+
+
+def ones(*args, **kwargs):
+    return th.ones(*args, **kwargs, device=dev())
+
+
+def arange(*args, **kwargs):
+    return th.arange(*args, **kwargs, device=dev())
+
+
+def NormedLinear(*args, scale=1.0, **kwargs):
+    """
+    nn.Linear but with normalized fan-in init
+    """
+    out = nn.Linear(*args, **kwargs)
+    out.weight.data *= scale / out.weight.norm(dim=1, p=2, keepdim=True)
+    if kwargs.get("bias", True):
+        out.bias.data *= 0
+    return out
+
+
+def LayerNorm(*args, **kwargs):
+    out = nn.LayerNorm(*args, **kwargs)
+    out.weight.no_scale = True
+    return out
+
+
+def flatten_image(x):
+    """
+    Flattens last three dims
+    """
+    *batch_shape, h, w, c = x.shape
+    return x.reshape((*batch_shape, h * w * c))
+
+
+def sequential(layers, x, *args, diag_name=None, use_checkpoint=False):
+    for i, layer in enumerate(layers):
+        x = layer(x, *args)
+    return x
+
+
+def save_kwargs(fn):
+    """
+    This decorator passes through the user-provided kwargs and adds one more, called
+    save_kwargs, mapping to {"create_fn" : name_of_decorated_fn, "kwargs" : other_kwargs}
+
+    You put on this decorator on a function that creates a pytorch module. This will
+    save the kwargs and the function that was used to create the module.
+    This lets us restore the model state later.
+    """
+
+    @functools.wraps(fn)
+    def wrapper(**kwargs):
+        if "save_kwargs" in kwargs:
+            return fn(**kwargs)
+        else:
+            sk = {**kwargs, "create_fn": f"{fn.__module__}:{fn.__name__}"}
+            return fn(save_kwargs=sk, **kwargs)
+
+    return wrapper
+
+
+def index(x, i):
+    """
+    Batched, broadcasting index of x along dimension i.ndim.
+
+    For example, if x has shape (1, 2, 3, 4, 5) and i has shape (1, 1, 3)
+    then the result has shape (1, 2, 3, 5) and each value in i must be between 0 and 3.
+    """
+    assert x.ndim >= i.ndim + 1
+    gather_dim = i.ndim
+    while i.ndim < x.ndim:
+        i = i.unsqueeze(-1)
+    expand_shape = list(x.shape)
+    expand_shape[gather_dim] = 1
+    i = i.expand(*expand_shape)
+    xi = th.gather(x, gather_dim, i)
+    assert xi.shape[gather_dim] == 1
+    return xi.squeeze(gather_dim)
